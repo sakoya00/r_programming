a62ac3cf9281af58a9b96b3d5fe8fc05d663f1ce
sakoya00 <65825897+sakoya00@users.noreply.github.com>
2020-06-10 19:58:36 -0500

    Add files via upload

---
+++ best.R
@@ -0,0 +1,52 @@
+best <- function(state, outcome) {
+  outcome2 <- data.frame()
+  outcome2 <- read.csv("C:/Users/sakoya00/Documents/outcome-of-care-measures.csv")
+  if(!state %in% state.abb){
+    stop("invalid state")
+  }
+  else if(!(identical(outcome, "heart attack")| identical(outcome, "heart failure") | identical (outcome, "pneumonia"))){
+    stop("invalid outcome")
+  }
+  else{
+    staterows <- subset(outcome2, outcome2$State == state)
+    lowhospital <- c()
+
+    
+    n <- integer()
+    if(identical(outcome, "heart attack"))
+      
+      n <- 11
+      
+    
+    
+    if(identical(outcome, "heart failure"))
+      
+      n <- 17
+      
+    
+    
+    if(identical(outcome, "pneumonia"))
+      
+      n <- 23
+      
+    
+    hospitalrow <- integer()
+    hospitalrow <- which(as.numeric(staterows[ ,n]) == min(as.numeric(staterows [,n]), na.rm= TRUE)) 
+    
+    min <- numeric()
+    min <- as.numeric(staterows [hospitalrow, n])
+    #must convert to numeric or it returns 10.1 as min of staterow[,23] for some reason
+    
+  }
+  lowhospital <- append (lowhospital, staterows [hospitalrow, 2])
+  
+  for(i in nrow(staterows)){
+    
+    if(identical(as.numeric(staterows[i, n]), min) & !identical(staterows[hospitalrow,2], staterows[i,2]))
+      
+      lowhospital <- append (lowhospital, staterows [i,2])
+    
+  }
+  return(min(lowhospital))
+}
+
